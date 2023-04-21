c3526c1a53930c22c1c4146b065549215d179182
no <no>
2023-04-21 12:30:57 -0400

    Reorganize

--- Grammar/CommandLine/Free.lua
+++ Grammar/CommandLine/Free.lua
@@ -2,15 +2,8 @@ local TF = require"Moonrise.Adapt.Transform"
 local String = require"Moonrise.Grammar.String"
 local BuiltinFilters = require"Moonrise.Grammar.Filters"
 
-local NYI = function() error"NYI" end;
-
 ---@type table<string, Adapt.Transform.Filter.Table>
-local Filters; Filters = {
-
-	LongSwitch = BuiltinFilters.Sequence(2);
-	ShortSwitches = BuiltinFilters.Sequence(2);
-	SettingValue = BuiltinFilters.Sequence(2);
-	Option = BuiltinFilters.Select;
+local Filters = {
 	Setting = {
 		Raise = function (Recurse, Argument)
 			local Success, Result = Recurse(Argument)
@@ -22,14 +15,13 @@ local Filters; Filters = {
 return TF.Grammar{
 	Value = String.Create(TF.Bytes(1)^1);
 	Key = (TF.Bytes(1) - TF.String"=")^1;
-	ShortSwitches = TF.String"-" * TF.Rule"Key";
-	LongSwitch = TF.String"--" * String.Create(TF.Rule"Key");
-	Option = (
-		  TF.Rule"LongSwitch" / Filters.LongSwitch 
-		+ TF.Rule"ShortSwitches" / Filters.ShortSwitches
-	) / Filters.Option;
+	ShortSwitches = TF.String"-" * TF.Rule"Key" / BuiltinFilters.Sequence(2);
+	LongSwitch = TF.String"--" * String.Create(TF.Rule"Key") / BuiltinFilters.Sequence(2);
+	Option = (TF.Rule"LongSwitch" + TF.Rule"ShortSwitches") / BuiltinFilters.Select;
 	(
 		  TF.Rule"Option" 
-		* ((TF.String"=" * TF.Rule"Value") / Filters.SettingValue)^-1
+		* (
+			(TF.String"=" * TF.Rule"Value") / BuiltinFilters.Sequence(2)
+		)^-1
 	) / Filters.Setting;
 }
